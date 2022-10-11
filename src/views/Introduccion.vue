7e28e85edf7dfa13f487b40002eaca2b70270ae7
Emilsen Bautista <emilse.a.b@gmail.com>
2022-10-11 01:48:50 -0500

    avances t1

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -2,13 +2,20 @@
 .curso-main-container.introduccion
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-  
-    .titulo-principal.color-acento-contenido
+
+    .titulo-principal.color-acento-contenido(data-aos="zoom-in-left")
       .titulo-principal__numero
         span
           i.fas.fa-info
       h1 Introducción
-    
+
+    p(data-aos="fade-down").mb-4 Estimado aprendiz, bienvenido al componente formativo #[strong “Clasificación aparatos dentales de ortopedia funcional”],  para iniciar visualice el siguiente video y conozca más:
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12(data-aos="fade-left")
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 </template>
 
 <script>
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,8 +5,16 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
-      
+      h1 Tipos de aparatos
+
+
+    .bg_img_01.p-5
+      .row.align-items-center.justify-content-start
+        .col-lg-6
+          p.my-5 Es importante resaltar que el técnico dental debe tener presente, al elaborar los diferentes aparatos de ortodoncia u ortopedia, la orden de trabajo, y estos se confeccionan teniendo en cuenta un conjunto de elementos que, al interactuar en boca, permitan no solo contener, sino, además, corregir la posición y función del sistema estomatognático, y que el profesional odontólogo es quien los instala y ajusta; por tal razón a continuación se presentan los diferentes dispositivos médicos sobre medida bucal:
+
+
+
 </template>
 
 <script>
