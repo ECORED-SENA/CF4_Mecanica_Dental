c90343f18be48a34f454606cbb828d5ba7e2dbb4
Emilsen Bautista <emilse.a.b@gmail.com>
2022-10-13 00:42:09 -0500

    se carga tema 1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -8,11 +8,61 @@
       h1 Tipos de aparatos
 
 
-    .bg_img_01.p-5
+    .bg_img_01.p-5.mb-5
       .row.align-items-center.justify-content-start
         .col-lg-6
           p.my-5 Es importante resaltar que el técnico dental debe tener presente, al elaborar los diferentes aparatos de ortodoncia u ortopedia, la orden de trabajo, y estos se confeccionan teniendo en cuenta un conjunto de elementos que, al interactuar en boca, permitan no solo contener, sino, además, corregir la posición y función del sistema estomatognático, y que el profesional odontólogo es quien los instala y ajusta; por tal razón a continuación se presentan los diferentes dispositivos médicos sobre medida bucal:
 
+    SlyderF(columnas="col-lg-6 col-xl-4")
+      .tarjeta.bg_azul.p-4
+        .row.justify-content-center.mb-3
+          .col-11
+            img(src='@/assets/curso/temas/tema1/img1.png' alt='AvatarTop')
+        h4.text-center Placa de Hawley
+        p.text-center Un retenedor de Hawley es una forma de retenedor de ortodoncia que utiliza una combinación de una placa de plástico duro o acrílico conectada por un alambre de metal delgado. El retenedor está moldeado a la boca para encajar cómodamente en el paladar, con el alambre asentado contra el frente de sus dientes superiores.
+
+
+      .tarjeta.bg_azul.p-4
+        .row.justify-content-center.mb-3
+          .col-11
+            img(src='@/assets/curso/temas/tema1/img2.png' alt='AvatarTop')
+        h4.text-center Hyrax
+        p.text-center El hyrax de ortodoncia es un aparato disyuntor de paladar cuya finalidad es la expansión rápida del maxilar superior. Los disyuntores de paladar son muy comunes en los tratamientos de ortodoncia interceptiva y su objetivo es corregir la mordida cruzada causada por un paladar estrecho.
+
+      .tarjeta.bg_azul.p-4
+        .row.justify-content-center.mb-3
+          .col-11
+            img(src='@/assets/curso/temas/tema1/img3.png' alt='AvatarTop')
+        h4.text-center  Tornillo transversal
+        p.text-center Los tornillos de expansión o placas de expansión son placas de resina con un tornillo expansor que tiene como objetivo aumentar la arcada dentaria, para recolocar los dientes en caso de apiñamiento.
+
+      .tarjeta.bg_azul.p-4
+        .row.justify-content-center.mb-3
+          .col-11
+            img(src='@/assets/curso/temas/tema1/img4.png' alt='AvatarTop')
+        h4.text-center   Mantenedor de espacio
+        p.text-center Es un tratamiento dental utilizado para mantener el espacio entre dientes cuando se pierde un diente temporal. Es decir, los mantenedores de espacio son recomendados cuando hay la pérdida prematura de los dientes temporales. Sirven para mantener el espacio desde que el diente temporal cae y erupciona el nuevo diente permanente. Si no se mantiene el espacio, los dientes temporales se mueven y se cierra el espacio que necesitan los dientes definitivos para erupcionar correctamente.
+
+      .tarjeta.bg_azul.p-4
+        .row.justify-content-center.mb-3
+          .col-11
+            img(src='@/assets/curso/temas/tema1/img5.png' alt='AvatarTop')
+        h4.text-center Tornillo Bertoni
+        p.text-center Tornillo de triple expansión o Bertoni es un tornillo que abre en tres direcciones de forma independiente. El tornillo tiene que ser cubierto en su totalidad por la resina.<br><br>Nota. Tomada de Laboshop (s .f.). Forestadent Bertoni - tornillos pluriseccionales.
+
+      .tarjeta.bg_azul.p-4
+        .row.justify-content-center.mb-3
+          .col-11
+            img(src='@/assets/curso/temas/tema1/img6.png' alt='AvatarTop')
+        h4.text-center Retenedor
+        p.text-center Los retenedores de dientes son aparatos bucales cuyo objetivo es, entre otros, estabilizar las nuevas posiciones de los dientes tras una ortodoncia minimizando los movimientos no deseados de las piezas dentales.<br><br>El retenedor fijo habitual es un alambre que se coloca en la parte interna de los dientes y, por tanto, es casi invisible.
+
+      .tarjeta.bg_azul.p-4
+        .row.justify-content-center.mb-3
+          .col-11
+            img(src='@/assets/curso/temas/tema1/img7.png' alt='AvatarTop')
+        h4.text-center Fraenkel
+        p.text-center Este es uno de los aparatos de ortopedia funcional más desarrollados de los últimos tiempos, se basa en la intercepción de problemas de la función muscular, no está diseñado para mover dientes ejerciendo presiones sobre ellos, sino que los libera y a sus estructuras basales de la presión muscular.<br><br> Nota. Tomada de Ortopus (2020). Fraenkel.
 
 
 </template>
@@ -35,4 +85,10 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+@media (max-width: 992px)
+  .bg_img_01
+    &::before
+      background-color: #ECEBF0 !important
+      background-image: none !important
+</style>
